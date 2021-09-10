ed701588fbb8ad3f06878419d18e947f531b33e9
Ryota Sasazawa <sasazawa3330@gmail.com>
2021-09-10 17:56:58 +0900

    could deploy cloud run revision

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -18,7 +18,51 @@ terraform {
 # Prepare Providers
 # -----------------------------------------------------------------------------
 provider "google" {
-  credentials = "${file("../../.credentials/terraform-service-account.json")}"
+  credentials = file("../../.credentials/terraform-service-account.json")
   project     = var.project_id
   region      = var.region
+}
+
+# -----------------------------------------------------------------------------
+# Enable APIs
+# -----------------------------------------------------------------------------
+resource "google_project_service" "project" {
+  service            = "run.googleapis.com"
+  project            = var.project_id
+  disable_on_destroy = true
+}
+
+# -----------------------------------------------------------------------------
+# Create Cloud Run
+# -----------------------------------------------------------------------------
+resource "google_cloud_run_service" "cloud_run" {
+  name     = "cloudrun-service"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image_id
+      }
+      service_account_name = google_service_account.cloud_run.email
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Cloud Runに紐付けるサービスアカウント
+resource "google_service_account" "cloud_run" {
+  account_id   = "cloud-run"
+  display_name = "Service Account"
+}
+
+# Cloud Runに紐付けるサービスアカウントにRoleを付与 
+resource "google_service_account_iam_member" "cloud_run_run_invoker" {
+  service_account_id = google_service_account.cloud_run.name
+  role               = "roles/editor"
+  member             = "serviceAccount:${google_service_account.cloud_run.email}"
 }
--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -6,3 +6,7 @@ variable "region" {
   type    = string
   default = "asia-northeast1"
 }
+
+variable "image_id" {
+  type = string
+}
